36343bf8192a2c780f5a47813a9ccf3127f8a200
Michael Thamm <mike.thamm@canonical.com>
2024-10-16 14:23:36 -0400

    Chore: Add integrations in outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,3 +1,17 @@
 output "app_name" {
   value = juju_application.catalogue.name
 }
+
+output "grafana_source_endpoint" {
+  description = "Name of the endpoint used by Catalogue to create a datasource in Grafana."
+  value       = "grafana-source"
+}
+
+output "ingress_endpoint" {
+  description = "Name of the endpoint used by Catalogue for the ingress configuration."
+  value       = "ingress"
+}
+
+output "catalogue_endpoint" {
+  value       = "catalogue"
+}
