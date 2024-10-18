59c24357ab1ef29fc198586b771a8839e674270f
Michael Thamm <mike.thamm@canonical.com>
2024-10-18 11:00:16 -0400

    Fix format, versions, and outputs for requires and provides

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -2,16 +2,16 @@ output "app_name" {
   value = juju_application.catalogue.name
 }
 
-output "grafana_source_endpoint" {
-  description = "Name of the endpoint used by Catalogue to create a datasource in Grafana."
-  value       = "grafana-source"
+output "requires" {
+  value = {
+    certificates = "certificates",
+    ingress      = "ingress",
+    tracing      = "tracing"
+  }
 }
 
-output "ingress_endpoint" {
-  description = "Name of the endpoint used by Catalogue for the ingress configuration."
-  value       = "ingress"
-}
-
-output "catalogue_endpoint" {
-  value       = "catalogue"
+output "provides" {
+  value = {
+    catalogue = "catalogue"
+  }
 }
